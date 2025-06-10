ecc3626e1d3b895ab38c534470aa609d15fc46d1
ali mahmoodi <alimahmoodi22@gmail.com>
2025-06-10 16:40:16 +0330

    Update IndexSectionOne.jsx

--- app_front/containers/IndexSectionOne/IndexSectionOne.jsx
+++ app_front/containers/IndexSectionOne/IndexSectionOne.jsx
@@ -4,45 +4,60 @@ import { mainDivIndxSecOne } from "./IndexSectionOneStyle";
 import LoginMain from "../Login/Login";
 import { useSelector } from "react-redux";
 
-const IndexSectionOne = () =>{
-    const auth = useSelector(state=>state.auth)
+const IndexSectionOne = () => {
+  const logged = useSelector(state => state.auth.logged ?? false);
 
-    const upperMainText = css`
-        width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: space-evenly;
-        flex-direction: column;
-        color: white;
-        height: 40%;
-    ` 
-    const mainTextH1 = css`
-        font-size: 4rem !important;
-        margin: 1rem;
-        animation:widthScalation 3s ease;
-        direction: rtl;
-        overflow: hidden;
-        color:white;
-    `
-    const subTextH3 = css`
-        font-size: 2rem;
-        margin: 1rem;
-        animation:widthScalation 5s ease;
-        direction: rtl;
-        overflow: hidden;
-        color:white;
-    `
-    return(
-        <div css={mainDivIndxSecOne}>
-            <div css={mainTextH1}>
-                شرکت نخل ارگ کریمان
-            </div>
-            <LoginMain/>
-            <div css={subTextH3}>
-                    اتوماسیون اداری
-            </div>  
-        </div>
-    )
-}
+  if (typeof logged === 'undefined') {
+    return <div>در حال بارگذاری...</div>;
+  }
 
-export default IndexSectionOne
+  const upperMainText = css`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: column;
+    color: white;
+    height: 100%;
+  `;
+  const mainTextH1 = css`
+    font-size: 4rem;
+    margin: 1rem;
+    animation: widthScalation 3s ease;
+    direction: rtl;
+    color: white;
+
+    @keyframes widthScalation {
+      from { width: 0; }
+      to { width: 100%; }
+    }
+  `;
+  const subTextH3 = css`
+    font-size: 2rem;
+    margin: 1rem;
+    animation: widthScalation 5s ease;
+    direction: rtl;
+    color: white;
+
+    @keyframes widthScalation {
+      from { width: 0; }
+      to { width: 100%; }
+    }
+  `;
+
+  return (
+    <div css={mainDivIndxSecOne}>
+      <div css={upperMainText}>
+        <h3 css={mainTextH1}>شرکت نخل ارگ کریمان</h3>
+        {!logged && (
+          <>
+            <LoginMain />
+            <h5 css={subTextH3}>اتوماسیون پروژه</h5>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default IndexSectionOne;
